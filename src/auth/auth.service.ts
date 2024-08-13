cb0ac997bc6e931839b203740720cf64d00dbd9f
mozadev <ceosmore@gmail.com>
2024-08-13 13:22:29 -0500

    chore: Refactor registerUser method in AuthService to handle user registration with bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,11 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { RpcException, MessagePattern } from '@nestjs/microservices';
 import { PrismaClient } from '@prisma/client';
+
+import * as bcrypt from 'bcrypt';
+
+
+import { RegisterUserDto } from './dto/register-user.dto';
 
 @Injectable()
 export class AuthService extends PrismaClient implements OnModuleInit {
@@ -9,4 +15,45 @@ export class AuthService extends PrismaClient implements OnModuleInit {
         this.$connect();
         this.logger.log('MongoDB connected')
     }
+
+    async registerUser(registerUserDto: RegisterUserDto) {
+
+        const { email, name, password } = registerUserDto;
+        try {
+            const user = await this.user.findUnique({
+                where: {
+                    email: email,
+                }
+            })
+
+            if (user) {
+                throw new RpcException({
+                    status: 400,
+                    message: 'User already exists'
+                })
+            }
+            const newUser = await this.user.create({
+                data: {
+                    email: email,
+                    password: bcrypt.hashSync(password, 10),
+                    name: name,
+                }
+            })
+
+            const { password: _, ...rest } = newUser;
+
+            return {
+                user: rest,
+                token: 'ABC'
+            }
+        } catch (error) {
+            throw new RpcException({
+                status: 400,
+                message: error.message
+            })
+        }
+    }
+
+
+
 }
